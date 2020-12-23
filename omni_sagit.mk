09b3b13a30229cd9d679009a2c1d9c02d38ced54
sbwml <984419930@qq.com>
2020-12-24 01:13:28 +0800

    sagit: twrp-9.0

---
+++ omni_sagit.mk
@@ -0,0 +1,38 @@
+#
+# Copyright 2017 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Release name
+PRODUCT_RELEASE_NAME := sagit
+
+$(call inherit-product, build/target/product/embedded.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.keystore=msm8998 \
+    ro.hardware.gatekeeper=msm8998
+
+# Parted
+PRODUCT_COPY_FILES += \
+    device/xiaomi/sagit/prebuilt/parted:$(PRODUCT_OUT)/recovery/root/sbin/parted
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := sagit
+PRODUCT_NAME := omni_sagit
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := MI 6
+PRODUCT_MANUFACTURER := Xiaomi
